c8bc2c72df8aaa008c4ec4ce4a930e714ebb1e7d
Benjamin Oakes <ben@benjaminoakes.com>
2012-03-15 16:22:45 -0400

    roboconf-npm

--- roboconf.sh
+++ roboconf.sh
@@ -27,6 +27,10 @@ function roboconf-bundler {
   bundle install $opts
 }
 
+function roboconf-npm {
+  npm install
+}
+
 function roboconf-rails-activerecord {
   bundle exec rake db:create
   bundle exec rake db:migrate
